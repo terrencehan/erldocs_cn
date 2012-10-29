770c202c77d20156199b1a7208e1e7772d9c8269
terrencehan <hanliang1990@gmail.com>
2012-10-29 21:40:31 +0800

    update file

--- script/xml2html.pl
+++ script/xml2html.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #author:terrencehan(hanliang1990@gmail.com)
-#This script is used to extract datas from .xml 
-#and .spec files and generate the responding .html 
+#This script is used to extract datas from .xml
+#and .spec files and generate the responding .html
 #files. If you find any bug, PLEASE report is to
 #my email :)
 
@@ -17,10 +17,13 @@ use 5.010;
 my $delete_en = 0;
 
 my $layout = read_file('./layout/default.html');
+my $app    = "";
 my %spec;
-my $erldocs_index = "var index = [";
+
+my @erldocs_index;
 
 my @xml_files;
+
 sub wanted {
     push @xml_files, $File::Find::name if $File::Find::name =~ /\.xml$/;
 }
@@ -28,24 +31,32 @@ find \&wanted, './xml_doc/';
 
 handle_a_file($_) for @xml_files;
 
+my $erldocs_index_str = dump \@erldocs_index;
+$erldocs_index_str =~ s/"(.*?)"/[$1]/g;
+write_file( './js/erldocs_index.js',
+    "var index = " . $erldocs_index_str . ";" );
+
 #script ends here, the followings are definitions of functions
 
 sub handle_a_file {
     my ($xml_file) = @_;
     my @path_pieses = split '/', $xml_file;
+
+    $app = $path_pieses[ $#path_pieses - 1 ];
+    my $temp = "'app', '$app', '$app', '[application]'";
+    push( @erldocs_index, $temp ) unless $temp ~~ @erldocs_index;
+
+    #generate spec file path by $xml_file
     my $spec_file =
       File::Spec->catfile( 'specs',
-        @path_pieses[ $#path_pieses - 1 .. $#path_pieses ] )
-      ;    #generate spec file path by $xml_file
-    $spec_file =~ s/xml/spec/;
+        @path_pieses[ $#path_pieses - 1 .. $#path_pieses ] );
     my $html_file =
-      File::Spec->catfile( @path_pieses[ $#path_pieses - 1 .. $#path_pieses ] )
-      ;    #generate html file path by $xml_file
+      File::Spec->catfile( @path_pieses[ $#path_pieses - 1 .. $#path_pieses ] );
+    $spec_file =~ s/xml/spec/;
     $html_file =~ s/xml/html/;
 
     my $spec_text = read_file($spec_file);
     my @specs = split /\n\n/, $spec_text;
-
     undef %spec;    #reset %spec for every file
     for (@specs) {
         if (/name:(?<name>.*?)\ndef:(?<def>.*?)\n(types:\n(?<types>.*))?/s) {
@@ -62,44 +73,57 @@ sub handle_a_file {
 }
 
 sub xml_to_html {
-    my $t = XML::Twig->new( pretty_print => 'indented' );
     my $xml_file = shift;
+    my $t = XML::Twig->new( pretty_print => 'indented' );
     $t->parsefile($xml_file);
     my $root = $t->root;
     $root->set_tag('div');
     $root->set_att( style => "margin:0px; padding:10px 20px" );
 
     my $module = $root->first_child('module');
-    if ( defined $module ) {
+    if ($module) {
         $module->set_tag('h1');
+        my $temp = "'mod', '$app', '${\$module->text}', []";
+        push( @erldocs_index, $temp ) unless $temp ~~ @erldocs_index;
     }
 
-    my $header = $root->first_child('header');
-    if ( defined $header ) {
+    if ( my $header = $root->first_child('header') ) {
         $header->delete;
     }
 
-    my $modulesummary = $root->first_child('modulesummary');
-    if ( defined $modulesummary ) {
+    if ( my $modulesummary = $root->first_child('modulesummary') ) {
         $modulesummary->set_tag('h2');
         $modulesummary->set_att( class => "modsummary" );
     }
 
-    my $description = $root->first_child('description');
-    if ( defined $description ) {
-        $description->set_tag('div');
-        $_->set_tag('p') for $description->children('p_zh');
+    ##################
+    #simple repalce
 
-        for ( $description->children('p') ) {
-            $_->set_tag('code') for $_->children('c');
+    $_->set_att( class => 'english' ) for ( $root->getElementsByTagName('p') );
+    $_->set_tag('p') for ( $root->getElementsByTagName('p_zh') );
+
+    if ($delete_en) {
+        for ( $root->getElementsByTagName('p') ) {
+            $_->delete;
         }
-        $description->set_att( class => "description" );
-
-        #$description->print;
     }
 
-    my $funcs = $root->first_child('funcs');
-    if ( defined $funcs ) {
+    $_->set_tag('span') for ( $root->getElementsByTagName('input') );
+
+    for ( $root->getElementsByTagName('code') ) {
+        $_->set_tag('pre');
+        $_->del_atts;
+    }
+    $_->set_tag('code') for $root->getElementsByTagName('c');
+
+    ##################
+
+    if ( my $description = $root->first_child('description') ) {
+        $description->set_tag('div');
+        $description->set_att( class => "description" );
+    }
+
+    if ( my $funcs = $root->first_child('funcs') ) {
         $funcs->set_tag('div');
         $funcs->set_att( class => "functions" );
 
@@ -116,15 +140,14 @@ sub xml_to_html {
             $func->set_tag('div');
             $func->set_att( class => "function" );
 
-            my $name = $func->first_child('name');
-            if ( defined $name ) {
+            #if ( my $name = $func->first_child('name') ) {
+            for my $name ( $func->children('name') ) {
                 if (    $name->att('arity')
                     and $name->att('name') )
                 {
                     my $name_name  = $name->att('name');
                     my $name_arity = $name->att('arity');
 
-                    #$name->delete;
                     my $str = $spec{ $name_name . '/' . $name_arity }->{def};
                     $name->set_inner_xml($str);
                     $name->set_att( id => $name_name . '/' . $name_arity );
@@ -134,6 +157,11 @@ sub xml_to_html {
                     $new_node->set_inner_xml($str);
                     $new_node->paste( after => $name );
                     $new_node->set_att( class => 'type_desc' );
+
+                    my $temp =
+"'fun', '$app', '${\$module->text}:$name_name/$name_arity', []";
+                    push( @erldocs_index, $temp )
+                      unless $temp ~~ @erldocs_index;
                 }
                 else {    #extract name and type in xml file directely
                     my $type = $func->first_child('type');
@@ -142,65 +170,37 @@ sub xml_to_html {
                         my $id = $+{fun_name} . '/' . scalar split /,/,
                           $+{args};
                         $name->set_att( id => $id );
+                        my $temp = "'fun', '$app', '${\$module->text}:$id', []";
+                        push( @erldocs_index, $temp )
+                          unless $temp ~~ @erldocs_index;
                     }
-                    for ( $type->children('v') ) {
-                        $_->set_tag('div');
-                        $_->set_att( class => 'type_desc' );
+                    if ($type) {
+                        for ( $type->children('v') ) {
+                            $_->set_tag('div');
+                            $_->set_att( class => 'type_desc' );
+                        }
                     }
                 }
 
                 $name->set_tag('h3');
             }
 
-            my $fsummary = $func->first_child('fsummary');
-            $fsummary->delete if defined $fsummary;
+            if ( my $fsummary = $func->first_child('fsummary') ) {
+                $fsummary->delete;
+            }
 
-            my $desc = $func->first_child('desc');
-            if ( defined $desc ) {
+            if ( my $desc = $func->first_child('desc') ) {
                 $desc->set_tag('div');
                 $desc->set_att( class => 'discription' );
 
-                for ( $desc->children('code') ) {
-                    $_->set_tag('pre');
-                    $_->del_atts;
-                }
-
-                for ( $desc->children('pre') ) {
-                    for ( $_->children('input') ) {
-                        $_->set_tag('span');
+                for my $block (qw/note warning/) {
+                    for ( $desc->children($block) ) {
+                        $_->set_tag('div');
+                        $_->set_att( class => $block );
+                        my $new_node = XML::Twig::Elt->new('h2');
+                        $new_node->set_inner_xml("${\ucfirst($block)}:<br/>");
+                        $new_node->paste($_);
                     }
-                }
-
-                eval { $_->delete for $desc->children('p') }
-                  if $delete_en;
-
-                $_->set_att( class => 'english' ) for $desc->children('p');
-                $_->set_tag('p') for $desc->children('p_zh');
-                for ( $desc->children('p') ) {
-                    $_->set_tag('code') for $_->children('c');
-                }
-
-                for ( $desc->children('warning') ) {
-                    $_->set_att( class => 'english' ) for $_->children('p');
-                    eval { $_->delete for $_->children('p') }
-                      if $delete_en;
-                    $_->set_tag('div');
-                    $_->set_att( class => 'warning' );
-                    my $new_node = XML::Twig::Elt->new('h2');
-                    $new_node->set_inner_xml("Warning:<br/>");
-                    $new_node->paste($_);
-                    $_->set_tag('p') for $_->children('p_zh');
-                }
-
-                for ( $desc->children('note') ) {
-                    eval { $_->delete for $_->children('p') }
-                      if $delete_en;
-                    $_->set_tag('div');
-                    $_->set_att( class => 'note' );
-                    my $new_node = XML::Twig::Elt->new('h2');
-                    $new_node->set_inner_xml("Note:<br/>");
-                    $new_node->paste($_);
-                    $_->set_tag('p') for $_->children('p_zh');
                 }
 
             }
